4f26cf46e67505899c13136e68fa1c9d5819b1f4
John Arheghan <ejdprogrammer@gmail.com>
2012-08-23 21:56:00 -0500

    files added aaron project

---
+++ AaronShoppingCart.Task/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AaronShoppingCart.Task
+{
+    public class Class1
+    {
+    }
+}
---
+++ AaronShoppingCart/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AaronShoppingCart.MvcApplication" Language="C#" %>
